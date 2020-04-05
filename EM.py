7b14c9a98d014c7b53c03ff64110f4d62bc22b79
akshay_bapat <abapat@terpmail.umd.edu>
2020-04-05 19:05:04 -0400

    Added final GMM models and updated EM

--- EM.py
+++ EM.py
@@ -2,12 +2,11 @@ import numpy as np
 import random
 import csv
 import math
+import yaml
 
 csv.field_size_limit(1000000000)
 
-GREEN = {}
-YELLOW = {}
-ORANGE = {}
+
 
 
 def generateCoVarMatrix(color):
@@ -30,110 +29,142 @@ def generateProb(givenInt, coVarMat, meanPt):
     return constant * math.exp(e)
 
 
-reader = csv.reader(open('green.csv', 'r'))
-for row in reader:
-    if row == []:
-        continue
-    else:
-        k, v = row
-        GREEN[k] = v
+def getGMM(colorClass):
 
-reader = csv.reader(open('yellow.csv', 'r'))
-for row in reader:
-    if row == []:
-        continue
-    else:
-        k, v = row
-        YELLOW[k] = v
+	DICT = {}
 
-reader = csv.reader(open('orange.csv', 'r'))
-for row in reader:
-    if row == []:
-        continue
-    else:
-        k, v = row
-        ORANGE[k] = v
-
-givenPt = np.array([[133], [230], [200]], dtype=float)
-
-# CoVarMatrix_Green = generateCoVarMatrix(GREEN)
-# meanPt_Green = generateMeanMatrix(GREEN)
-# #print("probability that the intensity is green: ", generateProb(givenPt, CoVarMatrix_Green, meanPt_Green))
-
-# CoVarMatrix_Yellow = generateCoVarMatrix(YELLOW)
-# meanPt_Yellow = generateMeanMatrix(YELLOW)
-# #print("probability that the intensity is yellow: ", generateProb(givenPt, CoVarMatrix_Yellow, meanPt_Yellow))
-
-# CoVarMatrix_Orange = generateCoVarMatrix(ORANGE)
-# meanPt_Orange = generateMeanMatrix(ORANGE)
-# #print("probability that the intensity is orange: ", generateProb(givenPt, CoVarMatrix_Orange, meanPt_Orange))
-
-
-
-pixels = []
-with open('Dataset_generation/yellow_buoy_dataset.csv', mode='r') as file:
-	reader = csv.reader(file, delimiter=',')
+	reader = csv.reader(open(colorClass + '.csv', mode='r'))
 	for row in reader:
-		pixels.append([int(row[0]), int(row[1]), int(row[2])])
+	    if row == []:
+	        continue
+	    else:
+	        k, v = row
+	        DICT[k] = v
 
-numPixels = len(pixels)
-pixels = np.array(pixels)
-numModels = 5
-threshold = []
-weights = []
-means = []
-covs = []
-for i in range(numModels):
-	weights.append(1/numModels)
-	means.append([])
-	covs.append(generateCoVarMatrix(YELLOW))
-	for j in range(3):
-		means[i].append(random.randint(0,255))
-		#covs[i].append([])
-		#for k in range(3):
-			#covs[i][j].append(random.randint(0,255))
-weights = np.array(weights)
-means = np.array(means)
-#covs = [np.array(covs)]
-#print(covs)
-#covs = [generateCoVarMatrix(GREEN), generateCoVarMatrix(GREEN)]
-#print(covs)
+	reader = csv.reader(open(colorClass + '.csv', mode='r'))
+	for row in reader:
+	    if row == []:
+	        continue
+	    else:
+	        k, v = row
+	        DICT[k] = v
 
-t = 11
-while(t > 10):
-	alphas = np.empty([numModels, numPixels])
-	denoms = np.zeros(numPixels)
+	reader = csv.reader(open(colorClass + '.csv', mode='r'))
+	for row in reader:
+	    if row == []:
+	        continue
+	    else:
+	        k, v = row
+	        DICT[k] = v
+
+	pixels = []
+	with open('Dataset_generation/' + colorClass + '_buoy_dataset.csv', mode='r') as file:
+		reader = csv.reader(file, delimiter=',')
+		for row in reader:
+			pixels.append([int(row[0]), int(row[1]), int(row[2])])
+
+	numPixels = len(pixels)
+	pixels = np.array(pixels)
+	numModels = 5
+	threshold = []
+	weights = []
+	means = []
+	covs = []
 	for i in range(numModels):
-		for j in range(numPixels):
-			alphas[i][j] = weights[i]*generateProb(pixels[j], covs[i], means[i])
-			#print(generateProb(pixels[j], covs[i], means[i]))
-			denoms[j] += alphas[i][j]
+		weights.append(1/numModels)
+		means.append([])
+		covs.append(generateCoVarMatrix(DICT))
+		for j in range(3):
+			means[i].append(random.randint(0,255))
+			#covs[i].append([])
+			#for k in range(3):
+				#covs[i][j].append(random.randint(0,255))
+	weights = np.array(weights)
+	means = np.array(means)
+
+	t = 11
+	while(t > 10):
+		alphas = np.empty([numModels, numPixels])
+		denoms = np.zeros(numPixels)
+		for i in range(numModels):
+			for j in range(numPixels):
+				alphas[i][j] = weights[i]*generateProb(pixels[j], covs[i], means[i])
+				#print(generateProb(pixels[j], covs[i], means[i]))
+				denoms[j] += alphas[i][j]
+
+		for i in range(numModels):
+			for j in range(numPixels):
+				alphas[i][j] /= denoms[j]
+
+		temp1 = np.zeros(3)
+		temp2 = np.zeros((3,3))
+		oldmeans = means.copy()
+		for i in range(numModels):
+			for j in range(numPixels):
+				temp1 += (alphas[i][j]*pixels[j]).reshape(3)
+				diff = pixels[j] - means[i]
+				temp2 += alphas[i][j]*(np.matmul([diff[:,None]], [diff])).reshape((3,3))
+
+			s = np.sum(alphas[i])
+			#print(alphas[i])
+			if s:
+				means[i] = temp1.copy()/s
+				weights[i] = s/numPixels
+				covs[i] = temp2.copy()/s
+
+		res = []
+		print(weights)
+		for i in range(numModels):
+			res.append(np.linalg.norm(means[i] - oldmeans[i]))
+		t = sum(res)
+	
+	data = dict(
+		weights = {},
+		means = {},
+		covMats = {},
+		)
 
 	for i in range(numModels):
-		for j in range(numPixels):
-			alphas[i][j] /= denoms[j]
+		data['weights'][i] = float(weights[i])
+		data['means'][i] = {}
+		data['covMats'][i] = {}
+		for j in range(3):
+			data['means'][i][j] = float(means[i][j])
+			data['covMats'][i][j] = {}
+			for k in range(3):
+				data['covMats'][i][j][k] = float(covs[i][j][k])
 
-	temp1 = np.zeros(3)
-	temp2 = np.zeros((3,3))
-	oldmeans = means.copy()
-	for i in range(numModels):
-		for j in range(numPixels):
-			temp1 += (alphas[i][j]*pixels[j]).reshape(3)
-			diff = pixels[j] - means[i]
-			temp2 += alphas[i][j]*(np.matmul([diff[:,None]], [diff])).reshape((3,3))
+	with open(colorClass + '_GMM.yaml', mode='w') as file:
+		yaml.dump(data, file)
 
-		s = np.sum(alphas[i])
-		#print(alphas[i])
-		if s:
-			means[i] = temp1.copy()/s
-			weights[i] = s/numPixels
-			covs[i] = temp2.copy()/s
 
-	res = []
-	print(weights)
-	for i in range(numModels):
-		res.append(np.linalg.norm(means[i] - oldmeans[i]))
-	t = sum(res)
-	#print(res)
-	#print(weights)
-#print(means, weights)
+def getProbGMM(currPixel, colorClass):
+	with open(colorClass + '_GMM.yaml', mode='r') as file:
+		GMM = yaml.safe_load(file)
+
+	covs = []
+	means = []
+	for i in range(len(GMM['weights'])):
+		means.append([])
+		covs.append([])
+		for j in range(3):
+			means[i].append(GMM['means'][i][j])
+			covs[i].append([])
+			for k in range(3):
+				covs[i][j].append(GMM['covMats'][i][j][k])
+
+	means = np.array(means)
+	covs = np.array(covs)
+
+	#print(means, covs)
+	p = 0
+	for i in range(len(GMM['weights'])):
+		p += generateProb(currPixel, covs[i], means[i])
+
+	print(p)
+
+# -----------------------------------------------------------------------------------------------------
+# HOW TO USE: getProbGMM([1, 1, 1], 'green')
+# -----------------------------------------------------------------------------------------------------
+#getGMM('orange')
+#print(GMM)
