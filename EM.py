78f34ce923b58d6f4809d4db221cf8ceee707095
akshay_bapat <abapat@terpmail.umd.edu>
2020-04-06 06:19:34 -0400

    Improved buoy detection

--- EM.py
+++ EM.py
@@ -9,162 +9,163 @@ GMM_Global = {}
 
 
 def generateCoVarMatrix(color):
-    rows = color['CoVar_Mat'][1: -1].split(']')
-    CoVarMatrix = np.array([rows[0][1:].split(','), rows[1][3:].split(','), rows[2][3:].split(',')], dtype=float)
-    return CoVarMatrix
+	rows = color['CoVar_Mat'][1: -1].split(']')
+	CoVarMatrix = np.array([rows[0][1:].split(','), rows[1][3:].split(','), rows[2][3:].split(',')], dtype=float)
+	return CoVarMatrix
 
 
 def generateMeanMatrix(color):
-    return np.array([color['B_Mean'], color['G_Mean'], color['R_Mean']], dtype=float)
+	return np.array([color['B_Mean'], color['G_Mean'], color['R_Mean']], dtype=float)
 
 
 def generateProb(givenInt, coVarMat, meanPt):
-    Det_CoVarMatrix = np.linalg.det(coVarMat)
-    constant = 1 / (((2 * 3.14) ** 3) * Det_CoVarMatrix) ** (0.5)
-    inv_CoVarMatrix = np.linalg.inv(coVarMat)
-    ptMinMean = givenInt - meanPt
-    e = np.matmul(np.matmul(ptMinMean, inv_CoVarMatrix), ptMinMean[:, None]) / (-2)
-    # print(np.matmul(ptMinMean, inv_CoVarMatrix))
-    return constant * math.exp(e)
+	Det_CoVarMatrix = np.linalg.det(coVarMat)
+	constant = 1 / (((2 * 3.14) ** 3) * Det_CoVarMatrix) ** (0.5)
+	inv_CoVarMatrix = np.linalg.inv(coVarMat)
+	ptMinMean = givenInt - meanPt
+	e = np.matmul(np.matmul(ptMinMean, inv_CoVarMatrix), ptMinMean[:, None]) / (-2)
+	# print(np.matmul(ptMinMean, inv_CoVarMatrix))
+	return constant * math.exp(e)
 
 
 def getGMM(colorClass):
-    DICT = {}
+	DICT = {}
 
-    reader = csv.reader(open(colorClass + '.csv', mode='r'))
-    for row in reader:
-        if row == []:
-            continue
-        else:
-            k, v = row
-            DICT[k] = v
+	reader = csv.reader(open(colorClass + '.csv', mode='r'))
+	for row in reader:
+		if row == []:
+			continue
+		else:
+			k, v = row
+			DICT[k] = v
 
-    reader = csv.reader(open(colorClass + '.csv', mode='r'))
-    for row in reader:
-        if row == []:
-            continue
-        else:
-            k, v = row
-            DICT[k] = v
+	reader = csv.reader(open(colorClass + '.csv', mode='r'))
+	for row in reader:
+		if row == []:
+			continue
+		else:
+			k, v = row
+			DICT[k] = v
 
-    reader = csv.reader(open(colorClass + '.csv', mode='r'))
-    for row in reader:
-        if row == []:
-            continue
-        else:
-            k, v = row
-            DICT[k] = v
+	reader = csv.reader(open(colorClass + '.csv', mode='r'))
+	for row in reader:
+		if row == []:
+			continue
+		else:
+			k, v = row
+			DICT[k] = v
 
-    pixels = []
-    with open('Dataset_generation/' + colorClass + '_buoy_dataset.csv', mode='r') as file:
-        reader = csv.reader(file, delimiter=',')
-        for row in reader:
-            pixels.append([int(row[0]), int(row[1]), int(row[2])])
+	pixels = []
+	with open('Dataset_generation/' + colorClass + '_buoy_dataset.csv', mode='r') as file:
+		reader = csv.reader(file, delimiter=',')
+		for row in reader:
+			pixels.append([int(row[0]), int(row[1]), int(row[2])])
 
-    numPixels = len(pixels)
-    pixels = np.array(pixels)
-    numModels = 5
-    threshold = []
-    weights = []
-    means = []
-    covs = []
-    for i in range(numModels):
-        weights.append(1 / numModels)
-        means.append([])
-        covs.append(generateCoVarMatrix(DICT))
-        for j in range(3):
-            means[i].append(random.randint(0, 255))
-        # covs[i].append([])
-        # for k in range(3):
-        # covs[i][j].append(random.randint(0,255))
-    weights = np.array(weights)
-    means = np.array(means)
+	numPixels = len(pixels)
+	pixels = np.array(pixels)
+	numModels = 3
+	threshold = []
+	weights = []
+	means = []
+	covs = []
+	for i in range(numModels):
+		weights.append(1 / numModels)
+		means.append([])
+		covs.append(generateCoVarMatrix(DICT))
+		for j in range(3):
+			means[i].append(random.randint(0, 255))
+		# covs[i].append([])
+		# for k in range(3):
+		# covs[i][j].append(random.randint(0,255))
+	weights = np.array(weights)
+	means = np.array(means)
 
-    t = 11
-    while (t > 10):
-        alphas = np.empty([numModels, numPixels])
-        denoms = np.zeros(numPixels)
-        for i in range(numModels):
-            for j in range(numPixels):
-                alphas[i][j] = weights[i] * generateProb(pixels[j], covs[i], means[i])
-                # print(generateProb(pixels[j], covs[i], means[i]))
-                denoms[j] += alphas[i][j]
+	t = 11
+	while (t > 1):
+		alphas = np.empty([numModels, numPixels])
+		denoms = np.zeros(numPixels)
+		for i in range(numModels):
+			for j in range(numPixels):
+				alphas[i][j] = weights[i] * generateProb(pixels[j], covs[i], means[i])
+				# print(generateProb(pixels[j], covs[i], means[i]))
+				denoms[j] += alphas[i][j]
 
-        for i in range(numModels):
-            for j in range(numPixels):
-                alphas[i][j] /= denoms[j]
+		for i in range(numModels):
+			for j in range(numPixels):
+				alphas[i][j] /= denoms[j]
 
-        temp1 = np.zeros(3)
-        temp2 = np.zeros((3, 3))
-        oldmeans = means.copy()
-        for i in range(numModels):
-            for j in range(numPixels):
-                temp1 += (alphas[i][j] * pixels[j]).reshape(3)
-                diff = pixels[j] - means[i]
-                temp2 += alphas[i][j] * (np.matmul([diff[:, None]], [diff])).reshape((3, 3))
+		temp1 = np.zeros(3)
+		temp2 = np.zeros((3, 3))
+		oldmeans = means.copy()
+		for i in range(numModels):
+			for j in range(numPixels):
+				temp1 += (alphas[i][j] * pixels[j]).reshape(3)
+				diff = pixels[j] - means[i]
+				temp2 += alphas[i][j] * (np.matmul([diff[:, None]], [diff])).reshape((3, 3))
 
-            s = np.sum(alphas[i])
-            # print(alphas[i])
-            if s:
-                means[i] = temp1.copy() / s
-                weights[i] = s / numPixels
-                covs[i] = temp2.copy() / s
+			s = np.sum(alphas[i])
+			# print(alphas[i])
+			if s:
+				means[i] = temp1.copy() / s
+				weights[i] = s / numPixels
+				covs[i] = temp2.copy() / s
 
-        res = []
-        print(weights)
-        for i in range(numModels):
-            res.append(np.linalg.norm(means[i] - oldmeans[i]))
-        t = sum(res)
+		res = []
+		print(weights)
+		for i in range(numModels):
+			res.append(np.linalg.norm(means[i] - oldmeans[i]))
+		t = sum(res)
 
-    data = dict(
-        weights={},
-        means={},
-        covMats={},
-    )
+	data = dict(
+		weights={},
+		means={},
+		covMats={},
+	)
 
-    for i in range(numModels):
-        data['weights'][i] = float(weights[i])
-        data['means'][i] = {}
-        data['covMats'][i] = {}
-        for j in range(3):
-            data['means'][i][j] = float(means[i][j])
-            data['covMats'][i][j] = {}
-            for k in range(3):
-                data['covMats'][i][j][k] = float(covs[i][j][k])
+	for i in range(numModels):
+		data['weights'][i] = float(weights[i])
+		data['means'][i] = {}
+		data['covMats'][i] = {}
+		for j in range(3):
+			data['means'][i][j] = float(means[i][j])
+			data['covMats'][i][j] = {}
+			for k in range(3):
+				data['covMats'][i][j][k] = float(covs[i][j][k])
 
-    with open(colorClass + '_GMM.yaml', mode='w') as file:
-        yaml.dump(data, file)
+	with open(colorClass + '_GMM.yaml', mode='w') as file:
+		yaml.dump(data, file)
 
 
 def getProbGMM(currPixel, colorClass):
-    if GMM_Global.get(colorClass, None) == None:
-        with open(colorClass + '_GMM.yaml', mode='r') as file:
-            GMM = yaml.safe_load(file)
+	if GMM_Global.get(colorClass, None) == None:
+		print("in")
+		with open(colorClass + '_GMM.yaml', mode='r') as file:
 
-        covs = []
-        means = []
-        for i in range(len(GMM['weights'])):
-            means.append([])
-            covs.append([])
-            for j in range(3):
-                means[i].append(GMM['means'][i][j])
-                covs[i].append([])
-                for k in range(3):
-                    covs[i][j].append(GMM['covMats'][i][j][k])
-        GMM_Global[colorClass] = {}
-        GMM_Global[colorClass]['length'] = len(GMM['weights'])
-        GMM_Global[colorClass]['means'] = np.array(means)
-        GMM_Global[colorClass]['covs'] = np.array(covs)
+			GMM = yaml.safe_load(file)
+		covs = []
+		means = []
+		for i in range(len(GMM['weights'])):
+			means.append([])
+			covs.append([])
+			for j in range(3):
+				means[i].append(GMM['means'][i][j])
+				covs[i].append([])
+				for k in range(3):
+					covs[i][j].append(GMM['covMats'][i][j][k])
+		GMM_Global[colorClass] = {}
+		GMM_Global[colorClass]['length'] = len(GMM['weights'])
+		GMM_Global[colorClass]['means'] = np.array(means)
+		GMM_Global[colorClass]['covs'] = np.array(covs)
 
-    # print(means, covs)
-    p = 0
-    for i in range(GMM_Global[colorClass]['length']):
-        p += generateProb(currPixel, GMM_Global[colorClass]['covs'][i], GMM_Global[colorClass]['means'][i])
+	# print(means, covs)
+	p = 0
+	for i in range(GMM_Global[colorClass]['length']):
+		p += generateProb(currPixel, GMM_Global[colorClass]['covs'][i], GMM_Global[colorClass]['means'][i])
 
-    return (p)
+	return (p)
 
 # -----------------------------------------------------------------------------------------------------
 # HOW TO USE: getProbGMM([1, 1, 1], 'green')
 # -----------------------------------------------------------------------------------------------------
-# getGMM('orange')
+#getGMM('green')
 # print(GMM)
--- generateBinaryVideo.py
+++ generateBinaryVideo.py
@@ -1,6 +1,7 @@
 import EM
 import numpy as np
 import cv2
+from imutils import contours
 
 
 cap = cv2.VideoCapture("detectbuoy.avi")
@@ -18,30 +19,53 @@ cap = cv2.VideoCapture("detectbuoy.avi")
 
 # points for GREEN (INSIDE)
 individualPixel = np.array([117, 181, 246])
-print(EM.getProbGMM(individualPixel, 'orange'))
+#print(EM.getProbGMM(individualPixel, 'orange'))
 individualPixel = np.array([113, 171, 247])
-print(EM.getProbGMM(individualPixel, 'orange'))
+#print(EM.getProbGMM(individualPixel, 'orange'))
 individualPixel = np.array([103, 153, 253])
-print(EM.getProbGMM(individualPixel, 'orange'))
+#print(EM.getProbGMM(individualPixel, 'orange'))
 individualPixel = np.array([107, 155, 251])
-print(EM.getProbGMM(individualPixel, 'orange'))
+#print(EM.getProbGMM(individualPixel, 'orange'))
 
 while cap.isOpened():
-    ret, frame = cap.read()
+	ret, frame = cap.read()
+	count = 0
+	out = frame.copy()
+	rows, cols, _ = frame.shape
+	binary = np.zeros((rows, cols, 3), np.float32)
+	for i in range(rows):
+		for j in range(cols):
+			individualPixel = frame[i, j]
+			temp = individualPixel[0]
+			individualPixel[0] = individualPixel[2]
+			individualPixel[2] = temp
+			#print(i,"x",j,":",individualPixel)
+			if (EM.getProbGMM(individualPixel, 'green') >= 3.00e-6):
+				binary[i,j] = np.array([255,255,255])
+			# else:
+			#	 frame[i, j] = np.array([255, 255, 255])
+	new = cv2.medianBlur(binary, 5)
+	#new = cv2.GaussianBlur(new,(5,5),5)
+	edges = cv2.Canny(np.uint8(new),50,255)
+	conts,h = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	(conts_sorted, boundingBoxes) = contours.sort_contours(conts, method="left-to-right")
+	hull = cv2.convexHull(conts_sorted[0])
+	(x,y),radius = cv2.minEnclosingCircle(hull)
+	if radius > 9:
+		cv2.circle(out,(int(x),int(y)),int(radius),(0,255,0),4)
 
-    rows, cols, _ = frame.shape
-    for i in range(rows):
-        for j in range(cols):
-            individualPixel = frame[i, j]
-            print(i,"x",j,":",individualPixel)
-            if (EM.getProbGMM(individualPixel, 'green') >= 1.00e-5):
-                frame[i,j] = np.zeros(3)
-            else:
-                frame[i, j] = np.array([255, 255, 255])
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(100000) & 0xFF == ord('q'):
-         break
-    break
-    # # When everything done, release the capture
-    # cap.release()
-    # cv2.destroyAllWindows()
+		#cv2.imshow("Final output",out)
+		cv2.imwrite('output/'+str(count)+'.png', out)
+		#images.append(out)
+	else:
+		#cv2.imshow("Final output",frame)
+		cv2.imwrite('output/'+str(count)+'.png', out)
+		#images.append(out)
+	#cv2.imshow('frame',edges)
+	count += 1
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	#break
+	# # When everything done, release the capture
+	# cap.release()
+	# cv2.destroyAllWindows()
